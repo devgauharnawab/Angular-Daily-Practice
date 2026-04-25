a4d8d27d192564d39485964a485cb6f67214f16f
gauharnawab <gauharnawab20@gmail.com>
2026-04-25 19:44:01 +0530

    performing interpolation and properties

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,17 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  templateUrl: './app.html',//we can replace the html file when needed
+  //template: `<h1>{{age}}</h1>`, //this is inline template
   styleUrl: './app.css'
 })
 export class App {
+  //these are called properties
+  //with the help of interpolation we can bind the properties in User Interface
   protected title = 'my-angular-app-day1';
+  age = 20;
+  name =  "Gauhar Nawab";
+  getName(){
+    return this.name;
+  }
 }
